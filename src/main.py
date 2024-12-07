66884282196f5489dffac0ce0b7ee78f8df59811
Ellipsis <rizkyfathur326@gmail.com>
2024-12-07 20:28:54 +0700

    chore(all): folder names

--- src/main.py
+++ src/main.py
@@ -70,9 +70,9 @@ class MainWindow(QMainWindow):
                     const links = {
                         "dashboard-link": ["components/dashboard/index.html", "components/dashboard/style.css"],
                         "cases-link": ["components/cases/index.html", "components/cases/style.css"],
-                        "suspects-link": ["components/suspect/index.html", "components/suspects/style.css"],
-                        "victims-link": ["components/victim/index.html", "components/victims/style.css"],
-                        "detectives-link": ["components/detective/index.html", "components/detectives/style.css"],
+                        "suspects-link": ["components/suspects/index.html", "components/suspects/style.css"],
+                        "victims-link": ["components/victims/index.html", "components/victims/style.css"],
+                        "detectives-link": ["components/detectives/index.html", "components/detectives/style.css"],
                         "settings-link": ["components/settings/index.html", "components/settings/style.css"]
                     };
 
